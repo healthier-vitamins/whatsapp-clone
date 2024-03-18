ff6fcc56ba17396c129f5c50dfa6135aabda9684
healthy-vitamins <cheeaudrian97@gmail.com>
2024-03-19 00:55:32 +0800

    feature: changed license

--- app-server/routes/index.ts
+++ app-server/routes/index.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import fs from 'fs'
 
 export default function setupRoutes(app: Application) {
-    // Dynamically load routes from the 'authentication' directory
+    // dynamically load routes from the 'authentication' directory
     const authenticationPath = path.join(__dirname, 'authentication')
     fs.readdirSync(authenticationPath).forEach((file) => {
         const route = require(path.join(authenticationPath, file))
